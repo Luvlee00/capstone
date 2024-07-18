539396e08dfb31130fe0aac5f47dddecc3dd0e5a
Luvlee00 <148864029+Luvlee00@users.noreply.github.com>
2024-07-18 23:25:30 +0800

    Add files via upload

--- Login/script.js
+++ Login/script.js
@@ -4,11 +4,11 @@
         const icon = this.querySelector('i');
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text';
-            icon.classList.remove('fa-eye');
-            icon.classList.add('fa-eye-slash');
-        } else {
-            passwordInput.type = 'password';
             icon.classList.remove('fa-eye-slash');
             icon.classList.add('fa-eye');
+        } else {
+            passwordInput.type = 'password';
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
         }
     });

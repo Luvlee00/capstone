0227d0392c2a1677616a2b9558d2899869e61409
Luvlee00 <148864029+Luvlee00@users.noreply.github.com>
2024-07-19 23:30:38 +0800

    Delete Personal Info directory

--- Personal Info/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('birthday').addEventListener('change', function () {
-    const birthday = new Date(this.value);
-    const ageDifMs = Date.now() - birthday.getTime();
-    const ageDate = new Date(ageDifMs);
-    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
-    document.getElementById('age').value = age;
-});
